1f49f24800a2c9b4020227e2bb6a6fd19e936614
Xcolma <dmitrijkorotkih820@gmail.com>
2026-06-17 00:58:48 +0400

    fix vapid keys

--- server/push.js
+++ server/push.js
@@ -2,11 +2,8 @@ const webpush = require("web-push");
 
 const vapidKeys = {
   publicKey:
-    process.env.VAPID_PUBLIC_KEY ||
-    "BEl62iIwu0C5PD0YqGj7c2qJz4YH5k0XkSMXgL7oRv6ZlL0k0c8dLwM3fYj9zQ5BvN3fR8tG7vB2cD4eF6gH8",
-  privateKey:
-    process.env.VAPID_PRIVATE_KEY ||
-    "l0H5nL3vR8tG7vB2cD4eF6gH8jK9mN1oP2qR3sT4uV5wX6yZ7",
+    "BE_g7Xn1J6PJGZPhPJQSFta3sOahYk_o0fCn1zjhDPflZ5VCv8Rgy0Fvoo6qj1SfROnus50AEQGlaD8faeAbDNY",
+  privateKey: "eDgMJ8X-swgAnW78EHn7wrescO_vylT9CYC7DmhSQ2g",
 };
 
 webpush.setVapidDetails(
